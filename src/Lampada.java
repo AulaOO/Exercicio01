406c223c0a63daa65e8b1cfe5887e1f43df2dfbd
LuisGuilhermeCipriai <cipriani.luisguilherme@gmail.com>
2018-09-03 15:23:53 -0300

    Criada classe Lampada

---
+++ src/Lampada.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Luis Guilherme
+ */
+public class Lampada {
+    private String ligada;
+    
+    public void ligar(){
+        
+    }
+    
+    public void desligar(){
+        
+    }
+    
+    public void verificarEstado(){
+        
+    }
+    
+    
+}
